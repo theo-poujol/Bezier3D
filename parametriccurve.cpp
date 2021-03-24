160a19312a4758a566df5a027479d49e04607fe6
theo-poujol <theo.poujol@etu.univ-amu.fr>
2021-03-24 15:23:34 +0100

    ParametricCurve creation

--- parametriccurve.cpp
+++ parametriccurve.cpp
@@ -1,6 +1,6 @@
 #include "parametriccurve.h"
 
-ParametricCurve::ParametricCurve()
+ParametricCurve::ParametricCurve(QVector<Point> control_pts,int control_x,int control_y, float red, float green, float blue)
 {
 
 }
--- parametriccurve.h
+++ parametriccurve.h
@@ -1,11 +1,32 @@
 #ifndef PARAMETRICCURVE_H
 #define PARAMETRICCURVE_H
 
+#include "point.h"
+#include <QVector>
 
 class ParametricCurve
 {
 public:
-    ParametricCurve();
+
+    /* CONSTRUCTEUR */
+
+    ParametricCurve(QVector<Point> control_pts,int control_x,int control_y, float red, float green, float blue);
+
+    /** FONCTIONS DES COURBES **/
+    void createCurve(QVector<GLfloat> *vertData);
+    void createControlSegment();
+    void swapGridSurface(bool showGrid);
+    void update();
+
+    /** SETTERS **/
+    void setControlPointColor();
+    void setStart(int start);
+    void setPrecision(int pas);
+    void setPoint(int numPoint,Point p);
+
+    /** GETTERS **/
+    Point getPoint(int numPoint);
+    int getStart(), getSize(), getSizeCurveParam();
 };
 
 #endif // PARAMETRICCURVE_H
